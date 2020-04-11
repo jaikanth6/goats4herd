4e9e012d45d5357ab124009cabdab60b61922307
Karthik NS <karthik_ns@live.com>
2020-04-11 13:06:22 -0700

    Exercise 2 complete

--- src/server/server.js
+++ src/server/server.js
@@ -22,6 +22,36 @@ http.listen(port, function () {
     console.log("Listening on port: " + port);
 });
 
+const stomps = 5;
+function MovePlayer(player, data) {
+    var deltaX = 0;
+    var deltaY = 0;
+    if (data.left) {
+        deltaX -= 1;
+    }
+    if (data.up) {
+        deltaY -= 1;
+    }
+    if (data.right) {
+        deltaX += 1;
+    }
+    if (data.down) {
+        deltaY += 1;
+    }
+
+    // NormalizeVec
+    const distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+
+    if (distance != 0) {
+        deltaX = deltaX / distance;
+        deltaY = deltaY / distance;
+    }
+
+    // ScaleVec
+    player.x += deltaX * stomps;
+    player.y += deltaY * stomps;
+}
+
 io.on("connection", function (socket) {
     socket.on("new player", function () {
         players[socket.id] = {
@@ -29,41 +59,30 @@ io.on("connection", function (socket) {
             y: 300,
             color: "hsl(" + 360 * Math.random() + ", 50%, 50%)",
             name: `dawg_${socket.id}`,
+            left: false,
+            right: false,
+            up: false,
+            down: false,
         };
     });
 
-    const stomps = 5;
     socket.on("movement", function (data) {
         var player = players[socket.id] || {};
 
-        var deltaX = 0;
-        var deltaY = 0;
-        if (data.left) {
-            deltaX -= 1;
-        }
-        if (data.up) {
-            deltaY -= 1;
-        }
-        if (data.right) {
-            deltaX += 1;
-        }
-        if (data.down) {
-            deltaY += 1;
-        }
-
-        // NormalizeVec
-        const distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
-
-        if (distance != 0) {
-            deltaX = deltaX / distance;
-            deltaY = deltaY / distance;
-        }
-
-        // ScaleVec
-        player.x += deltaX * stomps;
-        player.y += deltaY * stomps;
+        player.left = data.left;
+        player.right = data.right;
+        player.up = data.up;
+        player.down = data.down;
     });
 });
+
+// Physics
+setInterval(function () {
+    for (var id in players) {
+        var player = players[id];
+        MovePlayer(player, player);
+    }
+}, 15);
 
 setInterval(function () {
     io.sockets.emit("game-state", players);
